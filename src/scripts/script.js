9f08831258f07a965275f3a745b78df41406145f
Sergei Burdakin <serj-b94@yandex.ru>
2021-05-11 18:28:30 +0300

    Upcoming filters

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', getData);
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('g-search-btn').addEventListener('click',globalSearch);
-    document.getElementById('g-search').addEventListener('input',globalSearch);
+    document.getElementById('g-search-btn').addEventListener('click', globalSearch);
+    document.getElementById('g-search').addEventListener('input', globalSearch);
 });
 
 var data = null; //сделать через замыкание
-var result = [];
+var result = []; //сделать через замыкание
+var filterArray = []; //сделать через замыкание
 
 async function getJSON() {
     let result = await fetch('./src/api/data.json');
@@ -22,43 +23,49 @@ function renderTable(array) {
     clearTable();
     let length = Object.keys(array).length;
     let table = document.createElement('table');
-    table.setAttribute('id','table');
-    table.setAttribute('class','table');
+    table.setAttribute('id', 'table');
+    table.setAttribute('class', 'table');
     document.getElementById('results').appendChild(table);
     for (let i = 0; i < length; i++) {
         let row = document.createElement('tr');
-        row.setAttribute('class','row');
-        row.setAttribute('id',`row-${i}`);
+        row.setAttribute('class', 'row');
+        row.setAttribute('id', `row-${i}`);
         table.appendChild(row);
         let idNum = document.createElement('td');
-        idNum.setAttribute('dataset-td',`id-${i}`);
+        idNum.setAttribute('dataset-td', `id-${i}`);
         idNum.innerHTML = array[i].id;
         row.appendChild(idNum);
         let prior = document.createElement('td');
-        prior.setAttribute('dataset-td',`pr-${i}`);
+        prior.setAttribute('dataset-td', `pr-${i}`);
         prior.innerHTML = array[i].priority;
         row.appendChild(prior);
         let stat = document.createElement('td');
-        stat.setAttribute('dataset-td',`st-${i}`);
+        stat.setAttribute('dataset-td', `st-${i}`);
         stat.innerHTML = array[i].status;
         row.appendChild(stat);
         let num = document.createElement('td');
-        num.setAttribute('dataset-td',`num-${i}`);
+        num.setAttribute('dataset-td', `num-${i}`);
         num.innerHTML = array[i].series;
         row.appendChild(num);
         let start = document.createElement('td');
-        start.setAttribute('dataset-td',`start-${i}`)
+        start.setAttribute('dataset-td', `start-${i}`)
         start.innerHTML = array[i].start;
         row.appendChild(start);
         let fin = document.createElement('td');
-        fin.setAttribute('dataset-td',`fin-${i}`);
+        fin.setAttribute('dataset-td', `fin-${i}`);
         fin.innerHTML = array[i].finish;
         row.appendChild(fin);
         let by = document.createElement('td');
-        by.setAttribute('dataset-td',`by-${i}`);
+        by.setAttribute('dataset-td', `by-${i}`);
         by.innerHTML = array[i].by;
         row.appendChild(by);
         document.getElementById('results').appendChild(table);
+    }
+}
+
+function clearTable() {
+    if (document.getElementById('table') !== null) {
+        document.getElementById('table').remove();
     }
 }
 
@@ -74,8 +81,14 @@ function globalSearch() {
     renderTable(result);
 }
 
-function clearTable() {
-    if (document.getElementById('table') !== null) {
-        document.getElementById('table').remove();
+function orderSearch() {
+    let order = document.getElementById('order-num-text').value.toLowerCase;
+    if (order !== null || undefined) {
+        for (let key in data) {
+            let str = JSON.stringify(data[key].id).toLowerCase();
+            if (str.match(order) !== null) {
+                filterArray.push(data[key]);
+            }
+        }
+        renderTable(filterArray);
     }
-}
